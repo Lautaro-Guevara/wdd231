52a05784b2c5ea0f4d2e3ceab55ea01d49c26c49
Lautaro <lfguevara2001@gmail.com>
2024-10-02 16:59:15 -0300

    creating the weather api

---
+++ chamber/scripts/weather.js
@@ -0,0 +1,24 @@
+const currentTemperature = document.getElementById("current-temperature")
+
+// API Key: f5b917e2e73f63970c3b4fe848471f0d
+// -45.8714180210415, -67.47105453394504
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=-45.87&lon=-67.47&appid=f5b917e2e73f63970c3b4fe848471f0d"
+
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // testing only
+        displayResults(data); // uncomment when ready
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
