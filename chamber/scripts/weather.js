1f816ba58a68d129fb4950a9fafad3f04f32dc81
Lautaro <lfguevara2001@gmail.com>
2024-10-03 16:53:01 -0300

    added the displayResult function

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -21,4 +21,13 @@ async function apiFetch() {
     }
   }
   
-  apiFetch();
+apiFetch();
+
+function displayResults(data) {
+  currentTemperature.innerHTML = `${data._____}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${______}.___`;
+  let desc = data.weather[0].______;
+  //weatherIcon.setAttribute('___', _____);
+  //weatherIcon.setAttribute('___', _____);
+  captionDesc.textContent = `${desc}`;
+}
