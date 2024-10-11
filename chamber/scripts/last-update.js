a0d7ceb887466d07d6211459e200ea65aa5882fb
Lautaro-Guevara <lfguevara2001@gmail.com>
2024-10-11 17:38:57 -0300

    style updated

--- chamber/scripts/last-update.js
+++ chamber/scripts/last-update.js
@@ -5,3 +5,12 @@ const today = new Date();
 currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
 lastModified.innerHTML = `Last modification: <span class="highlight">${document.lastModified}</span>`;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
